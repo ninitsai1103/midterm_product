b761a3efac4057a57d0d5219ed529fe8167de7c7
YuFan <fan11657@hotmail.com>
2024-02-05 06:09:12 +0800

    修正 submit 會把 secondaryCategory 代進 url 的問題

--- product_pages/product-list.php
+++ product_pages/product-list.php
@@ -524,7 +524,7 @@ $rowsSecondaryCategory = $resultSecondaryCategory->fetch_all(MYSQLI_ASSOC);
                             <!-- 類別 -->
                             <form action="">
                                 <div class="d-flex">
-                                    <select class="form-select form-select-lg mb-3 me-2" aria-label="Large select example" name="primaryCategorySearch" onchange=this.form.submit()>
+                                    <select class="form-select form-select-lg mb-3 me-2" aria-label="Large select example" name="primaryCategorySearch" onchange="location.pathname + '?primaryCategorySearch=' + this.value">
                                         <option selected>主類別</option>
                                         <?php foreach ($rowsCategory as $primaryCategory) : ?>
                                             <? $isSelectedPrimary = $primaryCategory["id"] == $_GET["primaryCategorySearch"] ?>
