07774d1b9f9bd2e709699c7aaa49cd78cad97e11
Ni <daisytsai11336@gmail.com>
2024-02-05 17:00:37 +0800

    Demo問題修正

--- product_pages/add-product.php
+++ product_pages/add-product.php
@@ -1,5 +1,15 @@
 <?php
+if (!isset($_GET["id"])) {
+    $id = 0;
+} else {
+    $id = $_GET["id"];
+}
+// 連線資料庫
 require_once("../db_connect.php");
+// 選取資料
+$sqlAll = "SELECT * FROM product WHERE id=$id";
+$resultAll = $conn->query($sqlAll);
+$rowsAll = $resultAll->fetch_all(MYSQLI_ASSOC);
 // 抓主類別資料表
 $sqlCategory = "SELECT * FROM primary_category WHERE valid=1";
 $resultCategory = $conn->query($sqlCategory);
@@ -20,10 +30,21 @@ $rowsSecondaryCategory = $resultSecondaryCategory->fetch_all(MYSQLI_ASSOC);
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-    <!-- Bootstrap CSS v5.2.1 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+    <!-- Nucleo Icons -->
+    <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- CSS Files -->
+    <link id="pagestyle" href="../assets/css/argon-dashboard.css?v=2.0.4" rel="stylesheet" />
     <!-- Awesome Font -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Bootstrap JavaScript (Popper.js and Bootstrap JS) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="..." crossorigin="anonymous"></script>
+
 </head>
 
 <body>
@@ -37,34 +58,44 @@ $rowsSecondaryCategory = $resultSecondaryCategory->fetch_all(MYSQLI_ASSOC);
             </a>
         </div>
         <form action="doAddProduct.php" method="post" enctype="multipart/form-data">
+            <div class="mb-2 d-flex">
+                <select class="form-select form-select-lg mb-3 me-2" aria-label="Large select example" name="primaryCategorySelect" onchange="location.href='add-product.php?id=<?= $id ?>&primaryCategorySelect=' + this.value;">
+                    <option selected>主類別</option>
+                    <?php foreach ($rowsCategory as $primaryCategory) : ?>
+                        <?php if ($primaryCategory["id"] == $_GET["primaryCategorySelect"]) : ?>
+                            <option selected value="<?= $primaryCategory["id"] ?>"><?= $primaryCategory["name"] ?></option>
+                        <?php else : ?>
+                            <option value="<?= $primaryCategory["id"] ?>"><?= $primaryCategory["name"] ?></option>
+                        <?php endif; ?>
+                    <?php endforeach; ?>
+                </select>
+                <div>
+                    <a name="addPrimaryCategory" id="" class="btn btn-success" href="../product-category_pages/addPriCategory.php" role="button">
+                        <i class="fa-solid fa-plus fa-fw"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="mb-2 d-flex">
+                <select class="form-select form-select-lg mb-3 me-2" aria-label="Large select example" name="secondaryCategorySelect">
+                    <option selected>次類別</option>
+                    <?php foreach ($rowsSecondaryCategory as $secondaryCategory) : ?>
+                        <?php if ($secondaryCategory["primary_id"] == $_GET["primaryCategorySelect"]) : ?>
+                            <?php if ($secondaryCategory["id"] == $_GET["secondaryCategorySelect"]) : ?>
+                                <option selected value="<?= $secondaryCategory["id"] ?>"><?= $secondaryCategory["name"] ?></option>
+                            <?php else : ?>
+                                <option value="<?= $secondaryCategory["id"] ?>"><?= $secondaryCategory["name"] ?></option>
+                            <?php endif; ?>
+                        <?php endif; ?>
+                    <?php endforeach; ?>
+                </select>
+                <div>
+                    <a name="addSecondaryCategory" id="" class="btn btn-success" href="../product-category_pages/addSecCategory.php" role="button">
+                        <i class="fa-solid fa-plus fa-fw"></i>
+                    </a>
+                </div>
+            </div>
             <div class="mb-2">
                 <input type="text" class="form-control" name="name" placeholder="輸入商品名稱">
-            </div>
-            <div class="mb-2 d-flex">
-                <select class="form-select" aria-label="選擇主類別" name="primaryCategory">
-                    <option selected>選擇主類別</option>
-                    <?php foreach ($rowsCategory as $primaryCategory) : ?>
-                        <option value="<?= $primaryCategory["id"] ?>"><?= $primaryCategory["name"] ?></option>
-                    <?php endforeach; ?>
-                </select>
-                <div>
-                    <a name="addPrimaryCategory" id="" class="btn btn-success" href="../category_pages/addPriCategory.php" role="button">
-                        <i class="fa-solid fa-plus fa-fw"></i>
-                    </a>
-                </div>
-            </div>
-            <div class="mb-2 d-flex">
-                <select class="form-select" aria-label="選擇次類別" name="secondaryCategory">
-                    <option selected>選擇次類別</option>
-                    <?php foreach ($rowsSecondaryCategory as $secondaryCategory) : ?>
-                        <option value="<?= $secondaryCategory["id"] ?>"><?= $secondaryCategory["name"] ?></option>
-                    <?php endforeach; ?>
-                </select>
-                <div>
-                    <a name="addSecondaryCategory" id="" class="btn btn-success" href="../category_pages/addSecCategory.php" role="button">
-                        <i class="fa-solid fa-plus fa-fw"></i>
-                    </a>
-                </div>
             </div>
             <div class="mb-2 d-flex">
                 <input type="text" class="form-control me-2" name="price" placeholder="輸入價格">
--- product_pages/update-product.php
+++ product_pages/update-product.php
@@ -30,8 +30,21 @@ $rowsSecondaryCategory = $resultSecondaryCategory->fetch_all(MYSQLI_ASSOC);
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-    <!-- Bootstrap CSS v5.2.1 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+    <!-- Nucleo Icons -->
+    <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- CSS Files -->
+    <link id="pagestyle" href="../assets/css/argon-dashboard.css?v=2.0.4" rel="stylesheet" />
+    <!-- Awesome Font -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Bootstrap JavaScript (Popper.js and Bootstrap JS) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="..." crossorigin="anonymous"></script>
+
 </head>
 
 <body>
@@ -45,12 +58,6 @@ $rowsSecondaryCategory = $resultSecondaryCategory->fetch_all(MYSQLI_ASSOC);
                 <form action="doUpdateProduct.php" method="post" enctype="multipart/form-data">
                     <?php foreach ($rowsAll as $product) : ?>
                         <table class="table">
-                            <tr class="border-end">
-                                <th>商品名稱</th>
-                                <td>
-                                    <input type="text" class="form-control" name="name" value="<?= $product["name"] ?>">
-                                </td>
-                            </tr>
                             <tr class="border-end">
                                 <th>類別</th>
                                 <td>
@@ -76,6 +83,12 @@ $rowsSecondaryCategory = $resultSecondaryCategory->fetch_all(MYSQLI_ASSOC);
                                             <?php endif; ?>
                                         <?php endforeach; ?>
                                     </select>
+                                </td>
+                            </tr>
+                            <tr class="border-end">
+                                <th>商品名稱</th>
+                                <td>
+                                    <input type="text" class="form-control" name="name" value="<?= $product["name"] ?>">
                                 </td>
                             </tr>
                             <tr class="border-end">
